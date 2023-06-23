34b1d08b012e10b33eedcfb17385d79a92c33352
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-23 20:13:16 +0600

    added user signup functionality in the project

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,32 +1,30 @@
 const User = require("../models/user");
 const shortId = require("shortid");
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
   // console.log(req.body);
-  User.findOne({ email: req.body.email }).exec((err, user) => {
-    if (user) {
-      return res.status(400).json({
-        error: "Email is taken",
-      });
-    }
 
-    const { name, email, password } = req.body;
-    let username = shortId.generate();
-    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+  let user = await User.findOne({ email: req.body.email });
 
-    let newUser = new User({ name, email, password, profile, username });
-    newUser.save((err, success) => {
-      if (err) {
-        return res.status(400).json({
-          error: err,
-        });
-      }
-      // res.json({
-      //     user: success
-      // });
-      res.json({
-        message: "Signup success! Please signin.",
-      });
+  if (user) {
+    return res.status(400).json({
+      error: "Email is taken",
     });
-  });
+  }
+
+  const { name, email, password } = req.body;
+  let username = shortId.generate();
+  let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+  let newUser = new User({ name, email, password, profile, username });
+
+  try {
+    await newUser.save();
+    res.json({
+      message: "Signup success! Please signin.",
+      // user: newUser,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
