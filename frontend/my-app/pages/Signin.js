e3c646b5619fb1b68fa9484c6db0008f9d131e7f
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-28 00:40:16 +0600

    added signin and signup functionality in the project

---
+++ frontend/my-app/pages/Signin.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Signin = () => (
+  <>
+    <Layout>
+      <h1>Signin Page</h1>
+    </Layout>
+  </>
+);
+
+export default Signin;
---
+++ frontend/my-app/pages/Signup.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Signup = () => (
+  <>
+    <Layout>
+      <h1>Signup Page</h1>
+      <Link href="/">Home</Link>
+    </Layout>
+  </>
+);
+
+export default Signup;
