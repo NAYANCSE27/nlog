0206b1a665196727e01e63b0e34d87011e13941a
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-28 00:22:24 +0600

    done with configuration of the project

--- frontend/my-app/next.config.js
+++ frontend/my-app/next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "nlog",
+    API_DEVELOPMENT: "http://localhost:8000/api",
+    PRODUCTION: false,
+  },
+};
