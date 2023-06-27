222274d55a5cd70b1d65abb787c331471f1a5a06
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-27 16:35:51 +0600

    added bootstrap cdn link for the desing purpose

--- frontend/my-app/pages/index.js
+++ frontend/my-app/pages/index.js
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout";
 
 const Index = () => (
-  <Layout>
-    <h1>Index Page</h1>
-  </Layout>
+  <>
+    <Layout>
+      <h1>Index Page</h1>
+    </Layout>
+  </>
 );
 
 export default Index;
