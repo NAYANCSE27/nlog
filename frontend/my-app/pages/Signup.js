484cf676787148bd181d1ab1b34c2dabcab1ae96
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-30 23:10:11 +0600

    frontend for signup component done using bootstrap

--- frontend/my-app/pages/Signup.js
+++ frontend/my-app/pages/Signup.js
@@ -5,8 +5,12 @@ import SignupComponent from "../components/auth/SignupComponent";
 const Signup = () => (
   <>
     <Layout>
-      <h1>Signup Page</h1>
-      <SignupComponent />
+      <h1 className="text-center pt-4 pb-4">Signup</h1>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <SignupComponent />
+        </div>
+      </div>
     </Layout>
   </>
 );
