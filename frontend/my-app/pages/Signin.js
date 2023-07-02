442e91b8583b5831914fb9c7f2865091a63df001
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-07-02 21:37:20 +0600

    added signin functionality in the project and having some issues with the push method and import router from the next/router

--- frontend/my-app/pages/Signin.js
+++ frontend/my-app/pages/Signin.js
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
+import SigninComponent from "../components/auth/SigninComponent";
 
 const Signin = () => (
   <>
     <Layout>
-      <h1>Signin Page</h1>
+      <h1 className="text-center pt-4 pb-4">Signin</h1>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <SigninComponent />
+        </div>
+      </div>
     </Layout>
   </>
 );
